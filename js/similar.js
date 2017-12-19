84c5a13378df4c9f7677088d6ede8573a8011147
Igor Vladimirskiy <igor.vladimirskiy@wwpass.com>
2017-12-19 12:18:44 +0300

    Добавлены getRank, priceComparator, updateAdverts

--- js/similar.js
+++ js/similar.js
@@ -1,11 +1,50 @@
 'use strict';
 
 window.similar = function () {
+  var filters = document.querySelector('.map__filters');
   var loadPath = 'https://1510.dump.academy/keksobooking';
   var noticeForm = document.querySelector('.notice__form');
+  var filtersType;
+  var adverts = [];
+
+  filters.onTypeChange = function (type) {
+    filtersType = type;
+    window.util.debounce(updateAdverts);
+  };
+
+  function updateAdverts() {
+    window.fillAdvertTemplate.fillFragment(adverts.sort(function (left, right) {
+      var rankDiff = getRank(right) - getRank(left);
+      if (rankDiff === 0) {
+        rankDiff = pricesComparator(left.name, right.name);
+      }
+      return rankDiff;
+    }));
+  }
+
+  function getRank(advert) {
+    var rank = 0;
+
+    if (advert.type === filtersType) {
+      rank += 5;
+      console.log(rank);
+    }
+    return rank;
+  }
+
+  function pricesComparator(left, right) {
+    if (left > right) {
+      return 1;
+    } else if (left < right) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
 
   function successHandler(advertsArray) {
-    window.fillAdvertTemplate.fillFragment(advertsArray);
+    adverts = advertsArray;
+    updateAdverts(adverts);
     window.util.popupsHide();
   }
 
