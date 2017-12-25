a4cd8fc2e62e74321e944ab6bed7ec99b054554f
Igor Vladimirskiy <ivladimirskiy@ya.ru>
2017-12-25 22:24:36 +0300

    Исправление критерия Д12

--- js/show-card.js
+++ js/show-card.js
@@ -1,38 +1,40 @@
 'use strict';
 
-window.card = (function () {
-  var popupCloseIcon;
-  var mapPinPopup;
+(function () {
+  window.card = (function () {
+    var popupCloseIcon;
+    var mapPinPopup;
 
-  return {
-    showCard: function (e) {
-      var mapPinActive = 'map__pin--active';
-      if (e.target.closest('.map__pin:not(.map__pin--main)')) {
+    return {
+      showCard: function (e) {
+        var mapPinActive = 'map__pin--active';
+        if (e.target.closest('.map__pin:not(.map__pin--main)')) {
+          window.card.removePreviousActivePin(mapPinPopup);
+          window.util.addClassName(document.activeElement, mapPinActive);
+          mapPinPopup = document.activeElement.previousElementSibling;
+          popupCloseIcon = mapPinPopup.querySelector('.popup__close');
+          window.util.removeClassName(mapPinPopup, window.util.getHiddenAttribute());
+          popupCloseIcon.addEventListener('click', window.card.popupClose);
+          document.addEventListener('keydown', function (evt) {
+            window.util.isEscEvent(evt, window.card.popupClose);
+          });
+        }
+      },
+
+      popupClose: function () {
         window.card.removePreviousActivePin(mapPinPopup);
-        window.util.addClassName(document.activeElement, mapPinActive);
-        mapPinPopup = document.activeElement.previousElementSibling;
-        popupCloseIcon = mapPinPopup.querySelector('.popup__close');
-        window.util.removeClassName(mapPinPopup, window.util.getHiddenAttribute());
-        popupCloseIcon.addEventListener('click', window.card.popupClose);
-        document.addEventListener('keydown', function (evt) {
-          window.util.isEscEvent(evt, window.card.popupClose);
-        });
-      }
-    },
+      },
 
-    popupClose: function () {
-      window.card.removePreviousActivePin(mapPinPopup);
-    },
-
-    removePreviousActivePin: function (previousPopup) {
-      var pinActive = document.querySelector('.map__pin--active');
-      if (pinActive) {
-        window.util.removeClassName(pinActive, 'map__pin--active');
+      removePreviousActivePin: function (previousPopup) {
+        var pinActive = document.querySelector('.map__pin--active');
+        if (pinActive) {
+          window.util.removeClassName(pinActive, 'map__pin--active');
+        }
+        if (previousPopup) {
+          window.util.addClassName(previousPopup, window.util.getHiddenAttribute());
+          popupCloseIcon.removeEventListener('click', window.card.popupClose);
+        }
       }
-      if (previousPopup) {
-        window.util.addClassName(previousPopup, window.util.getHiddenAttribute());
-        popupCloseIcon.removeEventListener('click', window.card.popupClose);
-      }
-    }
-  };
+    };
+  })();
 })();
--- js/util.js
+++ js/util.js
@@ -1,67 +1,69 @@
 'use strict';
 
-window.util = (function () {
-  var disabled = 'disabled';
-  var fieldsetElements = document.querySelectorAll('.form__element');
-  var hidden = 'hidden';
-  var ESC_KEYCODE = 27;
-  var ENTER_KEYCODE = 13;
-  var numOfAdverts = 5;
-  var DEBOUNCE_INTERVAL = 500;
-  var lastTimeout;
+(function () {
+  window.util = (function () {
+    var disabled = 'disabled';
+    var fieldsetElements = document.querySelectorAll('.form__element');
+    var hidden = 'hidden';
+    var ESC_KEYCODE = 27;
+    var ENTER_KEYCODE = 13;
+    var numOfAdverts = 5;
+    var DEBOUNCE_INTERVAL = 500;
+    var lastTimeout;
 
-  return {
-    isEscEvent: function (evt, action) {
-      if (evt.keyCode === ESC_KEYCODE) {
-        action(evt);
+    return {
+      isEscEvent: function (evt, action) {
+        if (evt.keyCode === ESC_KEYCODE) {
+          action(evt);
+        }
+      },
+      isEnterEvent: function (evt, action) {
+        if (evt.keyCode === ENTER_KEYCODE) {
+          action(evt);
+        }
+      },
+      isEnterOrEscEvent: function (evt, action) {
+        if (evt.keyCode === ENTER_KEYCODE || evt.keyCode === ESC_KEYCODE) {
+          action(evt);
+        }
+      },
+      removeClassName: function (element, className) {
+        element.classList.remove(className);
+      },
+      addClassName: function (element, className) {
+        element.classList.add(className);
+      },
+      getRandomInt: function (min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+      },
+      formFieldsetHide: function () {
+        fieldsetElements.forEach(function (item) {
+          window.util.addClassName(item, disabled);
+        });
+      },
+      formFieldsetShow: function () {
+        fieldsetElements.forEach(function (item) {
+          window.util.removeClassName(item, disabled);
+        });
+      },
+      popupsHide: function () {
+        var popups = document.querySelectorAll('.popup');
+        popups.forEach(function (item) {
+          window.util.addClassName(item, hidden);
+        });
+      },
+      getHiddenAttribute: function () {
+        return hidden;
+      },
+      debounce: function (func) {
+        if (lastTimeout) {
+          window.clearTimeout(lastTimeout);
+        }
+        lastTimeout = window.setTimeout(func, DEBOUNCE_INTERVAL);
+      },
+      getNumberOfAdverts: function () {
+        return numOfAdverts;
       }
-    },
-    isEnterEvent: function (evt, action) {
-      if (evt.keyCode === ENTER_KEYCODE) {
-        action(evt);
-      }
-    },
-    isEnterOrEscEvent: function (evt, action) {
-      if (evt.keyCode === ENTER_KEYCODE || evt.keyCode === ESC_KEYCODE) {
-        action(evt);
-      }
-    },
-    removeClassName: function (element, className) {
-      element.classList.remove(className);
-    },
-    addClassName: function (element, className) {
-      element.classList.add(className);
-    },
-    getRandomInt: function (min, max) {
-      return Math.floor(Math.random() * (max - min)) + min;
-    },
-    formFieldsetHide: function () {
-      fieldsetElements.forEach(function (item) {
-        window.util.addClassName(item, disabled);
-      });
-    },
-    formFieldsetShow: function () {
-      fieldsetElements.forEach(function (item) {
-        window.util.removeClassName(item, disabled);
-      });
-    },
-    popupsHide: function () {
-      var popups = document.querySelectorAll('.popup');
-      popups.forEach(function (item) {
-        window.util.addClassName(item, hidden);
-      });
-    },
-    getHiddenAttribute: function () {
-      return hidden;
-    },
-    debounce: function (func) {
-      if (lastTimeout) {
-        window.clearTimeout(lastTimeout);
-      }
-      lastTimeout = window.setTimeout(func, DEBOUNCE_INTERVAL);
-    },
-    getNumberOfAdverts: function () {
-      return numOfAdverts;
-    }
-  };
+    };
+  })();
 })();
