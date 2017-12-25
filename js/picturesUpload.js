d39688cae39853a7ec2cf2fcef9687e213fbfd64
Igor Vladimirskiy <ivladimirskiy@ya.ru>
2017-12-25 21:53:05 +0300

    Убрана стилизация в picturesUpload.js

--- js/picturesUpload.js
+++ js/picturesUpload.js
@@ -3,29 +3,32 @@
 (function () {
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
   var avatarUploader = document.querySelector('.notice__photo .upload');
-  avatarUploader.style.position = 'relative';
   var avatarChooser = document.querySelector('#avatar');
-  avatarChooser.style.left = 0;
-  avatarChooser.style.opacity = 0;
-  avatarChooser.style.height = '100%';
   var photoContainer = document.querySelector('.form__photo-container');
   var list = document.createElement('ul');
-  list.style.display = 'flex';
-  list.style.listStyle = 'none';
   list.classList.add('advert-photos');
   photoContainer.appendChild(list);
   var advertPhotos = photoContainer.querySelector('.advert-photos');
   var uploadPhotos = document.querySelector('.form__photo-container .upload');
-  uploadPhotos.style.position = 'relative';
   var imagesChooser = document.querySelector('#images');
-  imagesChooser.style.left = 0;
-  imagesChooser.style.opacity = 0;
-  imagesChooser.style.height = '100%';
-  imagesChooser.style.width = '100%';
   var preview = document.querySelector('.notice__preview img');
 
   avatarChooser.addEventListener('change', upload);
   imagesChooser.addEventListener('change', uploadPhoto);
+
+  function setStylesElements() {
+    avatarUploader.style.position = 'relative';
+    avatarChooser.style.left = 0;
+    avatarChooser.style.opacity = 0;
+    avatarChooser.style.height = '100%';
+    list.style.display = 'flex';
+    list.style.listStyle = 'none';
+    uploadPhotos.style.position = 'relative';
+    imagesChooser.style.left = 0;
+    imagesChooser.style.opacity = 0;
+    imagesChooser.style.height = '100%';
+    imagesChooser.style.width = '100%';
+  }
 
   function upload() {
     var file = avatarChooser.files[0];
@@ -78,4 +81,6 @@
       reader.readAsDataURL(file);
     }
   }
+
+  setStylesElements();
 })();
